1ebd746dd6abb14018d408ddfd59158c884b20ee
NgocThao <Admin@DESKTOP-NK5P0MG>
2022-10-14 17:57:31 +0700

    Update phần còn lại buổi 20

--- src/webDriver/DriverManager.java
+++ src/webDriver/DriverManager.java
@@ -5,8 +5,11 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Navigation;
 import org.openqa.selenium.WebDriver.Options;
+import org.openqa.selenium.WebDriver.TargetLocator;
 import org.openqa.selenium.WebDriver.Timeouts;
+import org.openqa.selenium.WebDriver.Window;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,11 +19,12 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
-public abstract class DriverManager {
+public class DriverManager {
 //add
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 
+	@SuppressWarnings("deprecation")
 	@BeforeClass
 	public void beforeClass() {
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
@@ -28,23 +32,24 @@ public abstract class DriverManager {
 		driver= new ChromeDriver();
 		driver.manage().window(). maximize();
 		driver.get("https://www.facebook.com/");
-		driver.manage().timeouts().implicitlyWait(30 TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
 	}
 	
 	
+	@SuppressWarnings("deprecation")
 	@Test
 	public void TC_01_() {
 		
 		//Sẽ đóng tab đang đứng nếu có nhiều tab 
 		// Nếu chỉ có 1 tab thì đóng browser
-		driver.close();
+		driver.close();//*
 		//Không quan tâm bảo nhiêu tab => đóng browser
-		driver.quit();
+		driver.quit();//**
 		//- Có thể lưu nó vào 1 biến để sử dụng cho các step sau
 		//- Có thể sử dụng luôn (ko cần tạo biến)
 		
-		// cách này clear code hơn
+		// Tìm 1 element //**
 		WebElement emailTextbox =driver.findElement(By.xpath("//input[@id='email']"));
 		
 //		emailTextbox.clear();
@@ -58,10 +63,10 @@ public abstract class DriverManager {
 		// Có thể sử dụng luôn (ko cần tạo biến)
 		driver.findElement(By.xpath("//input[@id='email']")).click();
 		
-		//Tìm nhiều elements
+		//Tìm nhiều elements //*
 		List<WebElement> checkboxes= driver.findElements(By.xpath(projectPath));
 		
-				// Mở ra 1 url
+		// Mở ra 1 url //**
 		driver.get("https://www.facebook.com/");
 		
 		//Click vào link :Tiếng việt
@@ -84,10 +89,10 @@ public abstract class DriverManager {
 		//Trả về title của page hiện tại
 		Assert.assertEquals(driver.getTitle(),"Facebook");
 		
-//		Lấy ra được ID của tab hiện tại
+		//Lấy ra được ID của tab hiện tại //*
 		String loginWindowID = driver.getWindowHandle();
 		
-//		Lấy ra ID của tất cả các tab
+		//Lấy ra ID của tất cả các tab
 		String allIDs = driver.getWindowHandle();
 		
 		//Cookie/Cache
@@ -101,19 +106,35 @@ public abstract class DriverManager {
 		
 		Timeouts time = opt.timeouts();
 		
-		//Khonagr thời gian chờ 1 element xuất hiện
+		//khoảng thời gian chờ 1 element xuất hiện //**
 		time.implicitlyWait(15,TimeUnit.SECONDS);
 		time.implicitlyWait(5000,TimeUnit.MILLISECONDS);
 		time.implicitlyWait(5000000,TimeUnit.MICROSECONDS);
 		
-		//Khoảng thời gian chờ page lòa
+		
+		//Khoảng thời gian chờ page load
 		time.implicitlyWait(5,TimeUnit.SECONDS);
 		
 		Window win = opt.window();
+		win.fullscreen();
+		win.maximize(); //**
 		
-
-
 		
+		Navigation nav= driver.navigate();
+		nav.back(); 
+		nav.refresh();
+		nav.forward(); 
+		
+		nav.to("https://www.facebook.com/");
+		//thằng này = nghĩa với nav.to
+		//driver.get("https://www.facebook.com/");
+
+		TargetLocator tar = driver.switchTo(); 
+		tar.alert(); //*
+		
+		tar.frame("");
+		
+		tar.window("");
 	}
 
 }
